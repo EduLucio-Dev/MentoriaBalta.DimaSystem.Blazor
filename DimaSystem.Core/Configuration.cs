651e05293d33bf7934fe5b44a7728f23ff5f7e0f
EduLucio-Dev <eduardo.lucio@eisys.com.br>
2024-08-26 07:02:58 -0300

    - Criado o Response para paginação. - Criado as configurações de valores Default.

---
+++ DimaSystem.Core/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimaSystem.Core
+{
+    public class Configuration
+    {
+        public const int DefaultStatusCode = 200;
+        public const int DefaultPageCurrent = 1;
+        public const int DefaultPageNumber = 1;
+        public const int DefaultPageSize = 25;
+    }
+}
